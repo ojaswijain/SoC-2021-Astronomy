a3b0924c4d30c3c96525251776c32e247a0cf412
ojaswijain <81473548+ojaswijain@users.noreply.github.com>
2021-07-11 21:58:28 +0530

    Create redshift_plot.py

---
+++ Assignments/Grok/redshift_plot.py
@@ -0,0 +1,20 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Complete the following to make the plot
+if __name__ == "__main__":
+    data = np.load('sdss_galaxy_colors.npy')
+    # Get a colour map
+    cmap = plt.get_cmap('YlOrRd')
+
+    # Define our colour indexes u-g and r-i
+
+    # Make a redshift array
+
+    # Create the plot with plt.scatter and plt.colorbar
+
+    # Define your axis labels and plot title
+
+    # Set any axis limits
+
+    plt.show()
